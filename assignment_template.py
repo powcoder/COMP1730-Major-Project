dc20e6bdb75003bea3a9c14a7047d17dea430b1e
powcoder <powcoder@163.com>
2022-11-21 22:00:37 +0800

    COMP1730 Major Project

---
+++ assignment_template.py
@@ -0,0 +1,55 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+"""
+COMP1730/6730 S1 2020 - Project Assignment.
+
+Author: <your university ID here>
+"""
+
+import matplotlib.pyplot as plt
+from assignment_helpers import plot_volumes
+
+
+def read_dataset(filepath):
+    pass
+
+
+def largest_area(data):
+    pass
+
+
+def average_volume(data):
+    pass
+
+
+def most_average_rainfall(data):
+    pass
+
+
+def hottest_month(data):
+    pass
+
+
+def area_vs_volume(data):
+    areas = []
+    volumes = []
+
+    plt.plot(areas)
+    plt.plot(volumes)
+    plt.show()
+
+
+def lake_george_simple_model(data, evaporation_rate):
+    pass
+
+
+def lake_george_complex_model(data):
+    pass
+
+
+def evaluate_model(data, volumes):
+    pass
+
+
